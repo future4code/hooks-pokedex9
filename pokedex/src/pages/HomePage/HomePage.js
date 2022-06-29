e2f6b02ae8c7d79e2547ab5f76684ece101cc721
Dickson Freitas <dickfreitas12@gmaill.com>
2022-06-29 00:35:02 -0300

    Pokemons na tela

--- pokedex/src/pages/HomePage/HomePage.js
+++ pokedex/src/pages/HomePage/HomePage.js
@@ -3,28 +3,41 @@ import axios from "axios";
 import Header from "../../components/header/Header";
 import HomePageComponent from '../../components/homePage/ComponentHome'
 import { ContainerHome1 } from "./HomePageStyled";
-
 import styled from "styled-components";
 import GlobalStyle from "../../GlobalStyled";
 
-const HomePage = ()=>{
+const HomePage = (props)=>{
+   const [dataPokemon , setDataPokemon] = useState([])
+
+   const getPokemon = ()=>{
+    const url =" https://pokeapi.co/api/v2/pokemon/"
+
+        axios.get(url).then((res)=>{
+            setDataPokemon(res.data.results)
+            // console.log(res.data.results)
+            
+        }).catch((err)=>{
+            console.log(err.response)
+        })
+   }
+
+   const mapearPokemon = dataPokemon.map((pokemons,index)=>{
+        return <div key={index}>
+            <HomePageComponent pokemonsName={pokemons.name} indexPoke={index+1}/>
+            </div>
+        
+   })
+
     return(
         <div>
             <GlobalStyle/>
             <Header/>
             <ContainerHome1>
-            <HomePageComponent/>
-            <HomePageComponent/>
-            <HomePageComponent/>
-            <HomePageComponent/>
-            <HomePageComponent/>
-            <HomePageComponent/>
-
+            {mapearPokemon}
             </ContainerHome1>
-           
-
-           
-
+          
+          
+            
         </div>
     )
 }
