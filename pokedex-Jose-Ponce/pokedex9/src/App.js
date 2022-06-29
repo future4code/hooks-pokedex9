0e57288ecd9aebb46c9d60b7fe6d2205bff3c407
zeponce <jgponce@gmail.com>
2022-06-28 21:57:29 -0300

    DetailsPage-Ponce

---
+++ pokedex-Jose-Ponce/pokedex9/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DetailsPage from "./components/DetailsPage/DetailsPage";
+
+function App() {
+  return (
+    <>
+    <p>Pokedéx APP.js</p>
+    <DetailsPage/>
+    </>
+  );
+}
+
+export default App;
