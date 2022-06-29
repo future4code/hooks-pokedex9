fa59b113a5e12dae641da7e30266617965337ce5
GabrielePeron <gabrieleperon84@gmail.com>
2022-06-29 18:37:24 -0300

    acrescentado as rotas e linkado com botoes

--- pokedex/src/Router/Router.js
+++ pokedex/src/Router/Router.js
@@ -1,13 +1,14 @@
 import { BrowserRouter , Route ,  Routes  } from "react-router-dom";
 import HomePage from '../pages/HomePage/HomePage'
-
+import PokedexPage from "../pages/PokedexPage/PokedexPage";
+import DetailsPage from "../pages/DetailsPage/DetailsPage"
 
 const Router = () =>{
     return <BrowserRouter>
             <Routes>
                 <Route index element={<HomePage/>}/>
-                <Route/>
-                <Route/>
+                <Route path="pokedex" element={<PokedexPage />} />
+                <Route path="details" element={<DetailsPage />} />
             </Routes>
         </BrowserRouter>
     
