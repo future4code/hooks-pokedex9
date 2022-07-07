9d63790457682b03df241827f6e31ff10d325743
GabrielePeron <gabrieleperon84@gmail.com>
2022-07-07 15:33:20 -0300

    começado a estilizar pokedex, botão detalhes funciona remover ainda não

--- pokedex/src/Router/Router.js
+++ pokedex/src/Router/Router.js
@@ -1,7 +1,7 @@
 import { BrowserRouter , Route ,  Routes  } from "react-router-dom";
 import HomePage from '../pages/HomePage/HomePage'
 import PokedexPage from "../pages/PokedexPage/PokedexPage";
-import DetailsPage from "../pages/detailsPage/DetailsPage"
+import DetailsPage from "../pages/DetailsPage/DetailsPage"
 import HomePageComponent from "../components/homePage/ComponentHome";
 
 const Router = () =>{
--- pokedex/src/pages/PokedexPage/PokedexPageStyled.js
+++ pokedex/src/pages/PokedexPage/PokedexPageStyled.js
@@ -1,50 +1,87 @@
-import styled from "styled-components";
-
+import styled from 'styled-components'
 
 export const HeaderPokedex = styled.header`
-    
-    align-items: center;
-    height: 6em;
-    background-color: #38abed ;
-    max-width: 100vw;
-    color: yellow;
-    font-family: 'Bebas Neue', cursive;
-    font-size: medium;
-    margin: 0 auto;
-    height: 105px;
-    display: flex;
-    justify-content: space-between;
-    
-
+  align-items: center;
+  height: 6em;
+  background-color: #38abed;
+  max-width: 100vw;
+  color: yellow;
+  font-family: 'Bebas Neue', cursive;
+  font-size: medium;
+  margin: 0 auto;
+  height: 105px;
+  display: flex;
+  justify-content: space-between;
 `
-export const Titulo = styled.h2`
-    color: white;
-    font-family: cursive;
-    font-size: 24px;
-    margin: 65px;
-`    
+
 export const ButtonBack = styled.button`
-    display: flex;
-    font-family: cursive;
-    padding: 10px;
-    background-color: #1273DE;  
-    &:hover{ background: white}
-    &:hover{color: #1273DE};
-    color  : white ;
-    border-radius: 15px;
-    border: 1px solid black;
-    cursor: pointer;
-    margin:5px;
-    :hover{
-        -webkit-transform: scale(1.1);
-        -ms-transform: scale(1.1);
-        transform: scale(1.1);
-        transition:all 0.3s ease;
-    }
+  display: flex;
+  font-family: cursive;
+  padding: 10px;
+  background-color: #1273de;
+  &:hover {
+    background: white;
+  }
+  &:hover {
+    color: #1273de;
+  }
+  color: white;
+  border-radius: 15px;
+  border: 1px solid black;
+  cursor: pointer;
+  margin: 5px;
+  :hover {
+    -webkit-transform: scale(1.1);
+    -ms-transform: scale(1.1);
+    transform: scale(1.1);
+    transition: all 0.3s ease;
+  }
 `
 
-export const Footer = styled.footer`
-    min-height: 100vh;
-    min-width: 100vw;
-    background-color: #fff;
+export const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-left: 3em;
+  margin-top: 2em;
+  width: 20em;
+  height: 16em;
+  padding: 5px;
+  background-color: #38abed;
+  border: 1px black solid;
+  border-radius: 8px;
+`
+
+export const DivImage = styled.img`
+  width: 10em;
+  height: 10em;
+  padding: 0.5em;
+  background-color: white;
+  border-radius: 8px;
+`
+export const ButtonsPokedex = styled.button`
+  display: flex;
+  padding: 5px;
+  background-color: #1273de;
+  &:hover {
+    background: white;
+  }
+  &:hover {
+    color: #1273de;
+  }
+  color: white;
+  border-radius: 15px;
+  border: 1px solid black;
+  font-family: cursive;
+  cursor: pointer;
+  margin: 3px;
+`
+export const DivButtons = styled.div`
+  display: flex;
+  padding: 5px;
+`
+export const Name = styled.strong`
+  color: white;
+  font-family: cursive;
+  font-size: 20px;
 `
